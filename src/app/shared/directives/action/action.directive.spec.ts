33e1f22fd606411032fc84bce87f2d5583f24182
root <root@DesktopBruno.>
2024-11-26 22:44:41 -0300

    finalizado o test da diretiva

---
+++ src/app/shared/directives/action/action.directive.spec.ts
@@ -0,0 +1,40 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActionDirectiveModule } from './action.module';
+import { Component } from '@angular/core';
+
+describe('ActionDirective', () => {
+  let fixture: ComponentFixture<ActionDirectiveTestComponent>;
+  let component: ActionDirectiveTestComponent;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ActionDirectiveTestComponent],
+      imports: [ActionDirectiveModule],
+    }).compileComponents();
+    fixture = TestBed.createComponent(ActionDirectiveTestComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('(D) (@Output appAction) should emit event with payload when ENTER key is pressed', () => {
+    const divEl: HTMLElement =
+      fixture.nativeElement.querySelector('.dummy-component');
+    const event = new KeyboardEvent('keyUp', { key: 'Enter' });
+    divEl.dispatchEvent(event);
+    expect(component.hasEvent()).toBe(true);
+  });
+});
+
+@Component({
+  template: `<div
+    class="dummy-component"
+    (appAction)="actionHandler($event)"
+  ></div>`,
+})
+export class ActionDirectiveTestComponent {
+  private event: Event = null;
+  public actionHandler(event: Event): void {
+    this.event = event;
+  }
+  public hasEvent(): boolean {
+    return !this.event;
+  }
+}
